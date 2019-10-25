d974ddfbcf44b88230c3f3539e9afc0c6a693df7
Veronica Pop <opop@pitechplus.com>
2019-10-25 16:43:26 +0300

    Add build files

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+declare module 'react-tooltip-lite' {
+
+  import * as React from 'react';
+
+  export interface TooltipProps {
+    arrow?: boolean;
+    arrowSize?: number;
+    background?: string;
+    className?: string;
+    color?: string;
+    content: React.ReactNode;
+    contentParentId?: string;
+    direction?: string;
+    distance?: number;
+    eventOff?: string;
+    eventOn?: string;
+    eventToggle?: string;
+    forceDirection?: boolean;
+    hoverDelay?: number;
+    isOpen?: boolean;
+    mouseOutDelay?: number;
+    padding?: string;
+    styles?: object;
+    tagName?: string;
+    tipContentHover?: boolean;
+    tipContentClassName?: string;
+    useHover?: boolean;
+    useDefaultStyles?: boolean;
+  }
+
+  export default class Tooltip extends React.Component<TooltipProps> {
+  }
+
+}
